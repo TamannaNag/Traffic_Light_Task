1f6fbe2e93da9bc690802d57eee0c16de49eb695
TANNU <161803939+TamannaNag@users.noreply.github.com>
2024-09-17 11:54:50 +0530

    Add files via upload

---
+++ t.js
@@ -0,0 +1,18 @@
+function turnOnRed(){
+    document.getElementById('stopLight').style.backgroundColor = 'red';
+    document.getElementById('readyLight').style.backgroundColor = '#4b5069';
+    document.getElementById('goLight').style.backgroundColor = '#4b5069';
+
+}
+
+function turnOnYellow(){
+    document.getElementById('stopLight').style.backgroundColor = '#4b5069';
+    document.getElementById('readyLight').style.backgroundColor = 'yellow';
+    document.getElementById('goLight').style.backgroundColor = '#4b5069';
+}
+
+function turnOnGreen(){
+    document.getElementById('stopLight').style.backgroundColor = '#4b5069';
+    document.getElementById('readyLight').style.backgroundColor = '#4b5069';
+    document.getElementById('goLight').style.backgroundColor = 'green'
+}
